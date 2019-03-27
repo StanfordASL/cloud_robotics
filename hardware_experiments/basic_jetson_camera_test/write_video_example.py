a0f5bb0b64a94bc6d95ae0bab6ea63296b7d79ea
csandeep <csandeep@stanford.edu>
2019-03-27 12:53:57 -0700

    add a basic camera test for jetson tx2 on bootup

---
+++ hardware_experiments/basic_jetson_camera_test/write_video_example.py
@@ -0,0 +1,41 @@
+import numpy as np
+import sys
+
+try:
+    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+except:
+    print('no ROS')
+import cv2
+
+
+# whether to write an output video
+WRITE_OUT_VIDEO = False
+
+cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+
+
+if WRITE_OUT_VIDEO:
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+
+        if WRITE_OUT_VIDEO:
+            # write the unflipped frame
+            out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+
