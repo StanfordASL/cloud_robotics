7252c19b2da6a89281c09e1c5f5148650af8589f
csandeep90 <vss.emvr@gmail.com>
2019-03-27 11:30:43 -0700

    as of now, the jetson can stream and offload on python3, now just clean up the RL simulation code

--- hardware_experiments/offloader_socket_networking/Cloud_socket_server.sh
+++ hardware_experiments/offloader_socket_networking/Cloud_socket_server.sh
@@ -8,6 +8,10 @@ CLOUD_SVM_RESULTS_DIR=${DNN_MODEL_DIR}/${DNN_PREFIX}_CLOUD_SVM/
 
 echo $CLOUD_SVM_RESULTS_DIR
 
+HOST=192.168.1.101
+PORT=8000
+
 python3 video_central_facenet_socket_server.py  \
     --cloud_recognizer $CLOUD_SVM_RESULTS_DIR/recognizer.pickle \
     --cloud_le $CLOUD_SVM_RESULTS_DIR/le.pickle \
+    --host $HOST --port $PORT
