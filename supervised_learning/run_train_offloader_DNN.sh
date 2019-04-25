fca1ef1dd667bcb0c4f6dfe3dbdbb40979bc497d
csandeep90 <vss.emvr@gmail.com>
2019-04-25 16:09:15 -0700

    minor edits to offloader training script

--- supervised_learning/run_train_offloader_DNN.sh
+++ supervised_learning/run_train_offloader_DNN.sh
@@ -1,9 +1,14 @@
+# input data from videos in csv format to train on
 INPUT_TRAIN_CSV=$CLOUD_ROOT_DIR/data/video_data/total_train_df.csv
 
+# where to store the offloader model in KERAS
 OUTPUT_RESULTS_DIR=$CLOUD_ROOT_DIR/scratch_results/keras_offloader_DNN/
 
+# what is the column name we want to predict: whether to offload or not
 PREDICT_VAR='offload'
 
+# what input features do we use to decide if we should offload?
 TRAIN_FEATURES_LIST=$PWD/offloader_DNN_features/offloader_DNN_input_features.txt
 
+# train the offloader DNN
 python3 train_supervised_learning_offloader.py --output-results-dir $OUTPUT_RESULTS_DIR --input-train-csv $INPUT_TRAIN_CSV --predict-var $PREDICT_VAR --train-features-list $TRAIN_FEATURES_LIST
